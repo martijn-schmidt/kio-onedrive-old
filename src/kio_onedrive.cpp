20d1f96cd24bf8bceb69ff1b4dfd0b94e329f8d1
Martijn Schmidt <martijn.schmidt@gmail.com>
2018-01-07 23:50:34 +0100

    Declare const parentId in KIOOneDrive::del

--- src/kio_onedrive.cpp
+++ src/kio_onedrive.cpp
@@ -939,7 +939,7 @@ void KIOOneDrive::del(const QUrl &url, bool isfile)
     runJob(parentsFetch, url, accountId);
 
     if (parentsFetch.items().count() > 1) {
-        parentId = resolveFileIdFromPath(onedriveUrl.parentPath());
+        const parentId = resolveFileIdFromPath(onedriveUrl.parentPath());
         qCDebug(ONEDRIVE) << "More than one parent - deleting parentReference" << parentId << "from" << url;
         ParentReferenceDeleteJob parentDeleteJob(fileId, parentId, getAccount(accountId));
         runJob(parentDeleteJob, url, accountId);
